e7c9b6af788ffefae19a7689da61931644ea41cb
Miguel Natus Krummenauer <miguelnatus@gmail.com>
2025-09-25 22:09:37 -0300

    inquilinos

--- client_app/urls.py
+++ client_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from .views import HomeTenantView 
+from .views import HomeTenantView
+from django.contrib import admin 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', HomeTenantView.as_view(), name='home_tenant'),
 ]
