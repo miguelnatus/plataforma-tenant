755c6d4574f0fe6541c21cb2d675a1f4fae39d4c
Miguel Natus <miguelnatus@gmail.com>
2025-10-07 02:10:06 -0300

    anna landing

--- client_app/urls.py
+++ client_app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path 
 from .views import HomeTenantView
 from django.contrib import admin 
@@ -6,3 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeTenantView.as_view(), name='home_tenant'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- client_app/views.py
+++ client_app/views.py
@@ -3,18 +3,29 @@ from django.http import HttpResponse
 from django.views import View
 from .models import SiteSettings
 
-
 class HomeTenantView(View):
-
     def get(self, request):
+        # Idealmente, configurações por tenant (ex.: OneToOne com Tenant)
+        # settings_obj = SiteSettings.objects.filter(tenant=request.tenant).first()
         settings_obj = SiteSettings.objects.first()
 
-        for field in SiteSettings._meta.fields:
-            print(f"{field.name}: {getattr(settings_obj, field.name)}")
+        context = {
+            "settings": settings_obj,  # acessa no template como settings.logo, settings.primary_color etc.
+        }
+        return render(request, f'{request.tenant}/homeView.html', context)
+# class HomeTenantView(View):
 
-        # print("Site Settings:", settings_obj)
+#     def get(self, request):
+#         settings_obj = SiteSettings.objects.first()
 
-        return render(request, f'{request.tenant}/homeView.html')
+#         for field in SiteSettings._meta.fields:
+#             # print(f"{field.name}: {getattr(settings_obj, field.name)}")
+#             print(settings_obj.logo)
+#         # print("Site Settings:", settings_obj)
+
+#         return render(request, f'{request.tenant}/homeView.html')
+    
+   
 
 # def index(request):
 #     return HttpResponse(f"<h1>{request.tenant.schema_name} - {request.tenant.name}</h1>")
